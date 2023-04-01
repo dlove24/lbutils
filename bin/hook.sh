6dca128036533571280c4cc44857d8fe85049909
David Love <d.love@homeunix.uk>
2023-04-01 20:58:31 +0100

    Remove old document generation from the Git commit hook script

--- bin/hook.sh
+++ bin/hook.sh
@@ -5,8 +5,3 @@ black .
 
 # Check for obvious errors/style violations
 flake8
-
-# Regenerate the documentation. The module is
-# unlikely to be installed on the doc build machine
-# so read the files directly
-pdoc --output-dir docs lbutils !lbutils.examples
