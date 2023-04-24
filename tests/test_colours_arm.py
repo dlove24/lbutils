459e1c904a3f8b7ec457ef453c4293e110dfcbb7
David Love <d.love@homeunix.uk>
2023-04-24 20:41:47 +0100

    Test for both ARM and Intel bit order on the 'Colour' class

--- tests/test_colours_arm.py
+++ tests/test_colours_arm.py
@@ -29,12 +29,12 @@ report the correct binary representation, and the one recieved from the method.
 Run as: `py.test test_colours.py`
 """
 
-from lbutils.graphics.colours import Colour
+from lbutils.graphics.colours import DEVICE_BIT_ORDER, Colour
 
 
-def test_colour_white_565():
+def test_colour_white_565_arm():
     """Test that initialising the `Colour` class with Red = 255, Green =
-    255 and Blue = 255 gives the correct RGB565 response.
+    255 and Blue = 255 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
@@ -47,15 +47,15 @@ def test_colour_white_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(255, 255, 255)
+    colour = Colour(255, 255, 255, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
     assert bin_string == "1111111111111111"
     assert colour.as_rgb565 == 0xFFFF
 
 
-def test_colour_white_888():
+def test_colour_white_888_arm():
     """Test that initialising the `Colour` class with Red = 255, Green =
-    255 and Blue = 255 gives the correct RGB888 response.
+    255 and Blue = 255 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -68,16 +68,16 @@ def test_colour_white_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(255, 255, 255)
+    colour = Colour(255, 255, 255, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "111111111111111111111111"
-    assert colour.as_rgb888 == 0xFFFFFF
+    bin_string = f"{colour.as_rgb888:032b}"
+    assert bin_string == "11111111000000001111111111111111"
+    assert colour.as_rgb888 == 0xFF00FFFF
 
 
-def test_colour_black_565():
+def test_colour_black_565_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    0 and Blue = 0 gives the correct RGB565 response.
+    0 and Blue = 0 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
@@ -90,15 +90,15 @@ def test_colour_black_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 0, 0)
+    colour = Colour(0, 0, 0, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
     assert bin_string == "0000000000000000"
     assert colour.as_rgb565 == 0x0
 
 
-def test_colour_black_888():
+def test_colour_black_888_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    0 and Blue = 0 gives the correct RGB888 response.
+    0 and Blue = 0 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -111,16 +111,16 @@ def test_colour_black_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 0, 0)
+    colour = Colour(0, 0, 0, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "000000000000000000000000"
+    bin_string = f"{colour.as_rgb888:032b}"
+    assert bin_string == "00000000000000000000000000000000"
     assert colour.as_rgb888 == 0x0
 
 
-def test_colour_red_565():
+def test_colour_red_565_arm():
     """Test that initialising the `Colour` class with Red = 255, Green =
-    0 and Blue = 0 gives the correct RGB565 response.
+    0 and Blue = 0 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
@@ -133,15 +133,15 @@ def test_colour_red_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(255, 0, 0)
+    colour = Colour(255, 0, 0, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
-    assert bin_string == "1111100000000000"
-    assert colour.as_rgb565 == 0xF800
+    assert bin_string == "0000000011111000"
+    assert colour.as_rgb565 == 0x00F8
 
 
-def test_colour_red_888():
+def test_colour_red_888_arm():
     """Test that initialising the `Colour` class with Red = 255, Green =
-    0 and Blue = 0 gives the correct RGB888 response.
+    0 and Blue = 0 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -154,16 +154,16 @@ def test_colour_red_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(255, 0, 0)
+    colour = Colour(255, 0, 0, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "111111110000000000000000"
-    assert colour.as_rgb888 == 0xFF0000
+    bin_string = f"{colour.as_rgb888:032b}"
+    assert bin_string == "11111111000000000000000000000000"
+    assert colour.as_rgb888 == 0xFF000000
 
 
-def test_colour_green_565():
+def test_colour_green_565_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    255 and Blue = 0 gives the correct RGB565 response.
+    255 and Blue = 0 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
@@ -176,15 +176,15 @@ def test_colour_green_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 255, 0)
+    colour = Colour(0, 255, 0, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
-    assert bin_string == "0000011111100000"
-    assert colour.as_rgb565 == 0x07E0
+    assert bin_string == "1110000000000111"
+    assert colour.as_rgb565 == 0xE007
 
 
-def test_colour_green_888():
+def test_colour_green_888_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    255 and Blue = 0 gives the correct RGB888 response.
+    255 and Blue = 0 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -197,21 +197,21 @@ def test_colour_green_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 255, 0)
+    colour = Colour(0, 255, 0, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "000000001111111100000000"
-    assert colour.as_rgb888 == 0x00FF00
+    bin_string = f"{colour.as_rgb888:032b}"
+    assert bin_string == "00000000000000000000000011111111"
+    assert colour.as_rgb888 == 0x0000FF
 
 
-def test_colour_blue_565():
+def test_colour_blue_565_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    0 and Blue = 255 gives the correct RGB565 response.
+    0 and Blue = 255 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
 
-    **Pass**: Return value is 0x7E0
+    **Pass**: Return value is 0x1F00
 
     On-Failure
     ----------
@@ -219,15 +219,15 @@ def test_colour_blue_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 0, 255)
+    colour = Colour(0, 0, 255, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
-    assert bin_string == "0000000000011111"
-    assert colour.as_rgb565 == 0x001F
+    assert bin_string == "0001111100000000"
+    assert colour.as_rgb565 == 0x1F00
 
 
-def test_colour_blue_888():
+def test_colour_blue_888_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    0 and Blue = 255 gives the correct RGB888 response.
+    0 and Blue = 255 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -240,21 +240,21 @@ def test_colour_blue_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(0, 0, 255)
+    colour = Colour(0, 0, 255, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "000000000000000011111111"
-    assert colour.as_rgb888 == 0x0000FF
+    bin_string = f"{colour.as_rgb888:032b}"
+    assert bin_string == "00000000000000001111111100000000"
+    assert colour.as_rgb888 == 0x0000FF00
 
 
-def test_colour_beckett_565():
+def test_colour_beckett_565_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    255 and Blue = 0 gives the correct RGB565 response.
+    255 and Blue = 0 gives the correct RGB565 response for the ARM bit order.
 
     Expectation
     -----------
 
-    **Pass**: Return value is 0x7E0
+    **Pass**: Return value is 0x4D39
 
     On-Failure
     ----------
@@ -262,15 +262,15 @@ def test_colour_beckett_565():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(61, 41, 108)
+    colour = Colour(61, 41, 108, bit_order=DEVICE_BIT_ORDER.ARM)
     bin_string = f"{colour.as_rgb565:016b}"
-    assert bin_string == "0011100101001101"
-    assert colour.as_rgb565 == 0x394D
+    assert bin_string == "0100110100111001"
+    assert colour.as_rgb565 == 0x4D39
 
 
-def test_colour_beckett_888():
+def test_colour_beckett_888_arm():
     """Test that initialising the `Colour` class with Red = 0, Green =
-    255 and Blue = 0 gives the correct RGB888 response.
+    255 and Blue = 0 gives the correct RGB888 response for the ARM bit order.
 
     Expectation
     -----------
@@ -283,8 +283,8 @@ def test_colour_beckett_888():
       * Check the platform byte ordering has been correctly determined
       * Check the bit pattern returned against the expected in the method docstring
     """
-    colour = Colour(61, 41, 108)
+    colour = Colour(61, 41, 108, bit_order=DEVICE_BIT_ORDER.ARM)
 
-    bin_string = f"{colour.as_rgb888:024b}"
-    assert bin_string == "001111010010100101101100"
-    assert colour.as_rgb888 == 0x3D296C
+    bin_string = f"{colour.as_rgb888:032b}"
+    # assert bin_string == "00111101000000000110110000101001"
+    assert colour.as_rgb888 == 0x3D006C29
